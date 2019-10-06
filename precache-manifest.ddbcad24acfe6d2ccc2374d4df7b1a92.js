80bdad54394f196625b68d04d2bd5b303d067494
Guru081 <gurubasavaraj1996@gmail.com>
2019-10-06 22:22:01 +0530

    Updates

---
+++ precache-manifest.ddbcad24acfe6d2ccc2374d4df7b1a92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8937e7469d3a0ba0e2c3b58e262fd1d4",
+    "url": "/produ/index.html"
+  },
+  {
+    "revision": "7b80a315e6a636960fff",
+    "url": "/produ/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f960ed907addb1c39682",
+    "url": "/produ/static/js/2.5dfbc7b0.chunk.js"
+  },
+  {
+    "revision": "7b80a315e6a636960fff",
+    "url": "/produ/static/js/main.db599ea5.chunk.js"
+  },
+  {
+    "revision": "b3681b39d9dbb4236aca",
+    "url": "/produ/static/js/runtime-main.7f886765.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/produ/static/media/logo.25bf045c.svg"
+  }
+]);
